5125e0318eab951f1971efa0bc667f443eef78ed
jaehoon ahn <jaehun4086@naver.com>
2024-01-26 17:11:27 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,36 +1,36 @@
 function solution(progresses, speeds) {
     var answer = [];
-    let lated_date = [];
-    let count = 1;
-    let pan = [];
+    let work_end_date = [];
+    let number_of_deployment = 1;
+    let discrimination = [];
     
     
     for(let i = 0; i < progresses.length; i++){
-        lated_date.push(Math.ceil((100 - progresses[i]) / speeds[i]));
+        work_end_date.push(Math.ceil((100 - progresses[i]) / speeds[i]));
     }
     
-    for(let i = 0; i < lated_date.length; i++){
-        pan.push(true);
+    for(let i = 0; i < work_end_date.length; i++){
+        discrimination.push(true);
     }
-    for(let i = 0; i < lated_date.length - 1; i++){
-        for(let j = i + 1; j < lated_date.length; j++){
-            if(pan[i] === false){
+    for(let i = 0; i < work_end_date.length - 1; i++){
+        for(let j = i + 1; j < work_end_date.length; j++){
+            if(discrimination[i] === false){
                 break;
             }
-            if(lated_date[i] >= lated_date[j] && pan[j] !== false){
-                count += 1;
-                pan[j] = false;
+            if(work_end_date[i] >= work_end_date[j] && discrimination[j] !== false){
+                number_of_deployment += 1;
+                discrimination[j] = false;
             }
             else{
-                answer.push(count);
-                count = 1;
-                pan[i] = false;
+                answer.push(number_of_deployment);
+                number_of_deployment = 1;
+                discrimination[i] = false;
                 break;
             }
             
         }
     }
-    answer.push(count);
+    answer.push(number_of_deployment);
     
     return answer;
 }
