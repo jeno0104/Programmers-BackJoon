b8b6642e24f69cddac307aa13f3982471c8458b4
jaehoon ahn <jaehun4086@naver.com>
2024-06-25 14:13:23 +0900

    [level 1] Title: 모의고사, Time: 4.05 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,19 +1,23 @@
+
 def solution(answers):
-    result = []
-    end = []
-    jjic = [
-        [1, 2, 3, 4, 5],
-        [2, 1, 2, 3, 2, 4, 2, 5],
-        [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    ]
-    for i, pattern in enumerate(jjic):
-        count = 0
-        for j, answer in enumerate(answers):
-            if answer == pattern[j % len(pattern)]:
-                count += 1
-        result.append(count)
-    max_num = max(result)
-    for i, num in enumerate(result):
-        if max_num == num:
-            end.append(i + 1)
-    return end
+    man1 = [1, 2, 3, 4, 5]
+    man2 = [2, 1, 2, 3, 2, 4, 2 ,5]
+    man3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    result = [0 for i in range(3)]
+    answer = []
+    for i in range(len(answers)):
+        ans = answers[i]
+        if man1[i % len(man1)] == ans:
+            result[0] += 1 
+        if man2[i % len(man2)] == ans:
+            result[1] += 1 
+        if man3[i % len(man3)] == ans:
+            result[2] += 1 
+    
+    max_value = max(result)
+    
+    for i in range(len(result)):
+        if result[i] == max_value:
+            answer.append(i + 1)
+    return answer
