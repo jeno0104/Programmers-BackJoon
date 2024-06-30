1d51a4897166449cec5e33ffddee905158cd95a2
jaehoon ahn <jaehun4086@naver.com>
2024-07-01 00:26:11 +0900

    [Bronze II] Title: 문자열 반복, Time: 104 ms, Memory: 9360 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.js
@@ -0,0 +1,15 @@
+let fs = require("fs");
+
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let testCase = Number(input[0]);
+
+for(let i = 1; i <= testCase; i++){
+  let [r, s] = input[i].split(" ");
+  let answer = ""
+  
+  for(let j = 0; j <= s.length; j++){
+    answer += s.charAt(j).repeat(r);
+  }
+  console.log(answer);
+}
