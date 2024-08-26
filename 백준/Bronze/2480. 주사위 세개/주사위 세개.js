98db18e9ff1970745bf5c84a8add0b3f509ffac4
jaehoon ahn <jaehun4086@naver.com>
2024-08-26 14:15:06 +0900

    [Bronze IV] Title: 주사위 세개, Time: 92 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.js
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.js
@@ -1,20 +1,22 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let [a, b, c] = input[0].split(" ").map(Number);
+let [one, two, three] = input[0].split(" ").map(Number);
 
-if(a === b && b === c){
-  console.log(10000 + a * 1000);
+if(one === two && one === three && two === three){
+  console.log(10000 + one * 1000);
 }
-else if (a === b){
-  console.log(1000 + a * 100);
-}
-else if (b === c){
-  console.log(1000 + b * 100);
-}
-else if(a === c){
-  console.log(1000 + c * 100);
+else if(one !== two && one !== three && two !== three){
+  console.log(Math.max(one, two, three) * 100);
 }
 else{
-  console.log(Math.max(a, b, c) * 100);
+  if(one === two){
+    console.log(1000 + one * 100);
+  }
+  else if(one === three){
+    console.log(1000 + one * 100);
+  }
+  else if(two === three){
+    console.log(1000 + two * 100);
+  }
 }
