1f2962a91993a3344c3bd14876dcac7be0cf84ee
jaehoon ahn <jaehun4086@naver.com>
2024-08-26 14:07:56 +0900

    [Bronze III] Title: 오븐 시계, Time: 88 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.js
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.js
@@ -1,17 +1,16 @@
 let fs = require("fs");
-
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let h = Number(input[0].split(" ")[0]);
-let m = Number(input[0].split(" ")[1]);
-
+let [h, m] = input[0].split(" ").map(Number);
 let plusMinute = Number(input[1]);
 
 let totalMinute = h * 60 + m + plusMinute;
 
-totalMinute %= 1440;
+h = parseInt(totalMinute / 60);
+m = parseInt(totalMinute % 60);
+if(h > 23){
+  h -= 24;
+}
 
-let hour = parseInt(totalMinute / 60);
-let minute = totalMinute % 60;
 
-console.log(`${hour} ${minute}`);
+console.log(`${h} ${m}`);
