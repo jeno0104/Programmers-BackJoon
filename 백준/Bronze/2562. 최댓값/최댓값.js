ac9767acad62ec442c26e563b62bfb288d2fa9a1
jaehoon ahn <jaehun4086@naver.com>
2024-08-27 17:48:08 +0900

    [Bronze III] Title: 최댓값, Time: 88 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Bronze/2562. 최댓값/최댓값.js
+++ 백준/Bronze/2562. 최댓값/최댓값.js
@@ -1,16 +1,20 @@
 let fs = require("fs");
-
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let index = 0;
-let max = 0;
+
+let number = [];
 
 for(let i = 0; i < 9; i++){
-  if(max < Number(input[i])){
-    max = Number(input[i]);
-    index = i;
+  number.push(Number(input[i]));
+}
+
+let max = number.reduce((a, b) => Math.max(a, b));
+
+for(let i = 0; i < number.length; i++){
+  if(number[i] === max){
+    console.log(max);
+    console.log(i + 1);
+    break;
   }
 }
 
-console.log(max);
-console.log(index + 1);
