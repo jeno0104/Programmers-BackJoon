70cbf0fe59213d5b67eb3776bd7c7851c42bf90c
jaehoon ahn <jaehun4086@naver.com>
2024-08-27 16:59:49 +0900

    [Bronze V] Title: 합, Time: 92 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.js
+++ 백준/Bronze/8393. 합/합.js
@@ -1,10 +1,10 @@
 let fs = require("fs");
-
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let n = Number(input[0]);
-let sum = 0;
-for(let i = 1; i < n + 1; i++){
-  sum += i;
+let answer = 0;
+for(let i = 1; i <= n; i++){
+  answer += i;
 }
-console.log(sum);
+
+console.log(answer)
