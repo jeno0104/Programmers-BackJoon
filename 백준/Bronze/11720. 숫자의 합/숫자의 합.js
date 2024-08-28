8565ee4af628c13a7d6dae24dd634b0e77aa7c30
jaehoon ahn <jaehun4086@naver.com>
2024-08-28 15:17:04 +0900

    [Bronze IV] Title: 숫자의 합, Time: 92 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.js
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.js
@@ -1,14 +1,13 @@
 let fs = require("fs");
-
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let n = Number(input[0]);
 
-let number = input[1].split("").map(Number);
-
+let arr = input[1].split("");
 let sum = 0;
 
-for(let i = 0; i < n; i++){
-  sum += number[i];
+for(let i = 0; i < arr.length; i++){
+  sum += Number(arr[i]);
 }
+
 console.log(sum)
