0bfa00ffac43498d5cf03fdc7d49452b2a2b5f3b
jaehoon ahn <jaehun4086@naver.com>
2024-08-28 15:07:23 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 100 ms, Memory: 9736 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.js
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.js
@@ -1,24 +1,24 @@
 let fs = require("fs");
-
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let testCase = Number(input[0]);
+let n = Number(input[0]);
 
-for(let i = 1; i <= testCase; i++){
-  let data = input[i].split(" ").map(Number);
-  let n = data[0];
-  let summary = 0;
-  
-  for(let j = 1; j <= n; j++){
-    summary += data[j];
-  }
-  let average = summary / n;
+for(let i = 1; i <= n; i++){
+  let score = input[i].split(" ").map(Number);
+  let sum = 0; 
+  let avg = 0;
   let cnt = 0;
   
-  for(let k = 1; k <= n; k++){
-    if(data[k] > average){
+  for(let j = 1; j <= score[0]; j++){
+    sum += score[j];
+  }
+  avg = sum / score[0];
+
+  for(let j = 1; j <= score[0]; j++){
+    if(score[j] > avg){
       cnt += 1;
     }
-  };
-  console.log(`${(cnt / n * 100).toFixed(3)}%`)
+  }
+  
+  console.log(`${((100 / score[0]) * cnt).toFixed(3)}%`);
 }
