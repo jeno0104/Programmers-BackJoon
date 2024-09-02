1f9f7e6e845c80eee7097688ccb219eb9948ab89
jaehoon ahn <jaehun4086@naver.com>
2024-09-02 16:26:29 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 96 ms, Memory: 9340 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.js
@@ -1,19 +1,22 @@
 let fs = require("fs");
+
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let splitMinus = input[0].split("-");
 
-let answer = 0;
+let strings = input[0];
 
-for(let i = 0; i < splitMinus.length; i++){
-  let curr = splitMinus[i].split("+").map(Number).reduce((a, b) => a + b);
-
-  if(i === 0){
-    answer += curr;
-  }
-  else{
-    answer -= curr;
-  }
+let newStrings = strings.split("-");
+let values = [];
+for(let i = 0; i < newStrings.length; i++){
+  let news = newStrings[i].split("+").map(Number);
+  let sum = news.reduce((a, b) => a + b);
+  values.push(sum);
 }
 
-console.log(answer)
+if(values.length === 1){
+  console.log(values[0]);
+}
+else{
+  let result = values.reduce((a, b) => a + b);
+  console.log(values[0] * 2 - result);
+}
