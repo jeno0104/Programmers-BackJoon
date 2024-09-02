e67d1f5374b2a4f634353c3520a55eb16a3c4fc3
jaehoon ahn <jaehun4086@naver.com>
2024-09-02 15:27:05 +0900

    [Silver IV] Title: 동전 0, Time: 96 ms, Memory: 9612 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.js
+++ 백준/Silver/11047. 동전 0/동전 0.js
@@ -1,18 +1,23 @@
+// let fs = require("fs");
+// let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 let fs = require("fs");
 
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let [n, money] = input[0].split(" ").map(Number);
+let [n, k] = input[0].split(" ").map(Number);
+
 let coins = [];
 
 for(let i = 1; i <= n; i++){
-  coins.push(Number(input[i]));
-}
-let answer = 0;
-
-for(let i = n - 1; i >= 0; i--){
-  answer += parseInt(money / coins[i]);
-  money %= coins[i];
+  let coin = Number(input[i]);
+  coins.push(coin);
 }
 
-console.log(answer)
+coins.sort((a, b) => b - a);
+let result = 0;
+
+for(let coin of coins){
+  result += parseInt(k / coin);  
+  k %= coin; 
+}
+console.log(result);
