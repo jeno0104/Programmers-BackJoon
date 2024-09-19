e6a0a5e55d6dcb93af5a3079e2d503901e84c78e
jaehoon ahn <jaehun4086@naver.com>
2024-09-19 21:59:25 +0900

    [level 1] Title: 약수의 합, Time: 0.29 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,10 +1,9 @@
 function solution(n) {
-    var answer = 0;
     let arr = [];
-    for(let i = 1; i < n + 1; i++){
-        if(n % i === 0){
-            answer += i;
-        }
+    
+    for(let i = 1; i <= n; i++){
+        if(n % i === 0) arr.push(i);
     }
+    let answer = arr.reduce((a, b) => a + b, 0) 
     return answer;
 }
