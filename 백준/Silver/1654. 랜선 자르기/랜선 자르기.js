1db22e60a7eced1a99974c200afbfcee484c42b6
jaehoon ahn <jaehun4086@naver.com>
2024-10-03 15:40:53 +0900

    [Silver II] Title: 랜선 자르기, Time: 172 ms, Memory: 15020 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.js
@@ -2,33 +2,29 @@ let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let [n, k] = input[0].split(" ").map(Number);
-let lines = [];
 
-for(let i = 1; i <= n; i++){
-  let line = Number(input[i]);
-  lines.push(line);
+let arr = [];
+
+for (let i = 1; i <= n; i++) {
+  let a = Number(input[i]);
+  arr.push(a);
 }
-
-let maxLine = lines.reduce((a, b) => Math.max(a, b));
-let start = 0;
-let end = maxLine;
+let start = 1;
+let end = Math.max(...arr);
 let result = 0;
 
-while(start <= end){
-  let mid = parseInt((start + end) / 2);  
-  let totalLine = 0;
-  
-  for(let i = 0; i < n; i++){
-    totalLine += parseInt(lines[i] / mid);
+while (start <= end) {
+  let mid = parseInt((start + end) / 2);
+  let summary = 0;
+
+  for (let x of arr) {
+    if (x >= mid) summary += parseInt(x / mid);
   }
-  
-  if(totalLine < k){
+  if (summary < k) {
     end = mid - 1;
-  }
-  else{
-    start = mid + 1; 
-    result = mid;
+  } else {
+    start = mid + 1;
+    result = Math.max(result, mid);
   }
 }
-
-console.log(result)
+console.log(result);
