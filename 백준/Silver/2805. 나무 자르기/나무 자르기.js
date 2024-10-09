2dfce1cfa6e22dd29675deb550e5b4527b579975
jaehoon ahn <jaehun4086@naver.com>
2024-10-09 13:19:02 +0900

    [Silver II] Title: 나무 자르기, Time: 564 ms, Memory: 137036 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.js
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -2,25 +2,28 @@ let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let [n, m] = input[0].split(" ").map(Number);
-let namu = input[1].split(" ").map(Number);
+let arr = input[1].split(" ").map(Number);
 
-let start = 0;
-let end = namu.reduce((a, b) => Math.max(a, b));
-let result = 0;
+let start = 1;
+let end = Math.max(...arr);
+let answer = 0;
 
-while(start <= end){
+while (start <= end) {
   let mid = parseInt((start + end) / 2);
-  let total = 0;
-  
-  for(let i = 0; i < namu.length; i++){
-    if(namu[i] > mid){
-      total += namu[i] - mid;
+  let summary = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    let newX = arr[i] - mid;
+    if (newX > 0) {
+      summary += newX;
     }
   }
-  if(total < m) end = mid - 1;
-  else{
-    result = mid;
+  if (summary >= m) {
+    answer = Math.max(answer, mid)
     start = mid + 1;
+  } 
+  else{
+    end = mid - 1;
   }
 }
-console.log(result);
+console.log(answer);
