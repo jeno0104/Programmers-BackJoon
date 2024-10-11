8f2c49ebc5a5d6a38ca27448788b0ebb04f94cbd
jaehoon ahn <jaehun4086@naver.com>
2024-10-11 14:22:59 +0900

    [Silver III] Title: N과 M (1), Time: 288 ms, Memory: 41272 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -2,33 +2,35 @@ let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
 let [n, m] = input[0].split(" ").map(Number);
-let arr = [];
-for(let i = 1; i <= n; i++) arr.push(i);
+
 let visited = new Array(n).fill(false);
+let arr = [];
+let answer = [];
 let selected = [];
 
-let answer = "";
+for(let i = 1; i <= n; i++) arr.push(i);
 
 function dfs(arr, depth){
   if(depth === m){
     let result = [];
-    for(let i of selected) result.push(arr[i]);
-    for(let x of result) answer += x + " ";
+    for(let i of selected) result.push(i);
+    for(let x of result){
+      answer += x + 1 + " ";
+    }
     answer += "\n";
     return;
   }
   for(let i = 0; i < arr.length; i++){
-    
-    if(visited[i]){
-      continue;
-    } 
-      
+    if(visited[i]) continue;
     selected.push(i);
     visited[i] = true;
     dfs(arr, depth + 1);
     selected.pop();
     visited[i] = false;
   }
+
+  
 }
+
 dfs(arr, 0);
-console.log(answer)
+console.log(answer);
