8220075c3fc80d4738866864e78550c2d730e095
jaehoon ahn <jaehun4086@naver.com>
2024-10-17 11:51:44 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,10 +1,7 @@
+-- 코드를 입력하세요
 SELECT A.ANIMAL_ID, A.NAME
 FROM ANIMAL_OUTS A
 LEFT JOIN ANIMAL_INS B
 ON A.ANIMAL_ID = B.ANIMAL_ID
 WHERE B.ANIMAL_ID IS NULL
-
-# INNER JOIN은 교집합을 구하는 것
-# 유실된 데이터를 확인할 수 없음 
-# -> LEFT JOIN을 해야함
-# LEFT 조인은 왼쪽은 그대로 있고, 오른쪽이 왼쪽으로 들어간다.
+ORDER BY A.ANIMAL_ID
