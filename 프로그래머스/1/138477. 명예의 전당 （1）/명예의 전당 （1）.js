991a2e6119aa2106542475d2efe3a4d725e2b475
jaehoon ahn <jaehun4086@naver.com>
2024-10-17 15:51:26 +0900

    [level 1] Title: 명예의 전당 (1), Time: 8.86 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -1,46 +1,28 @@
 function solution(k, score) {
-    var myoungye = [];
-    let answer = [];
+    var answer = [];
+    let result = [];
     
-    for(let i = 0 ; i < score.length; i++){
-        myoungye.push(score[i]);
-        myoungye = myoungye.sort((a, b) => a - b).reverse();
-        if(myoungye.length > k){
-            myoungye.pop()
+    for(let i = 0; i < score.length; i++){
+        let flag = true;
+        let enter = answer.length === k ? false : true
+        for(let j = 0; j < k; j++){
+            if(enter){
+                answer.push(score[i]);
+                result.push(Math.min(...answer));
+                break;
+            }
+            else if(answer[j] <= score[i]){
+                answer.push(score[i]);
+                answer.sort((a, b) => b - a);
+                answer.pop();
+                result.push(answer[answer.length - 1]);
+                flag = false;
+                break;
+            }
         }
-        answer.push(myoungye[myoungye.length - 1]);
-        
+        if(flag && !enter){
+            result.push(answer[answer.length - 1]);
+        }
     }
-  
-//     myoungye.push(score[0]);
-//     answer.push(score[0]);
-//     if(myoungye[0] <= score[1]){
-//         myoungye.splice(0, 0, score[1]);
-//     }
-//     else{
-//         myoungye.splice(1, 0, score[1]);
-//     }
-//     answer.push(myoungye[myoungye.length - 1]);
-//     for(let i = 2; i < score.length; i++){
-//         for(let j = 0; j < k; j++){
-//             if(myoungye[j] === undefined){
-//                 break;
-//             }
-//             if(myoungye[j] < score[i]){
-//                 myoungye.splice(j, 0, score[i]);
-//                 break;
-//             }
-//             else if(myoungye[j] >= score[i] && myoungye[j + 1] <= score[i]){
-//                 myoungye.splice(j + 1, 0, score[i]);
-//                 break;
-//             }
-                
-//         }
-//         if(myoungye.length > k){
-//             myoungye.pop()
-//         }        
-//         answer.push(myoungye[myoungye.length - 1]);
-//     }
-    
-    return answer;
+    return result;
 }
