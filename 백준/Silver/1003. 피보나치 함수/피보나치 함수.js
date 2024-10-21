0766db2bab61984c622ea85a945bf8ce89e81f38
jaehoon ahn <jaehun4086@naver.com>
2024-10-21 14:29:01 +0900

    [Silver III] Title: 피보나치 함수, Time: 96 ms, Memory: 9876 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.js
@@ -1,18 +1,19 @@
 let fs = require("fs");
 let input = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-d = new Array(100).fill(0);
-d[0] = 0;
-d[1] = 1;
+let dp = new Array(100).fill(0);
 
-for(let i = 2; i <= 40; i++){
-  d[i] = d[i - 1] + d[i - 2];
+dp[0] = 0;
+dp[1] = 1;
+
+for(let i = 2; i < 100; i++){
+  dp[i] = dp[i - 1] + dp[i - 2];
 }
+
 let testCase = Number(input[0]);
 
-for(let t = 1; t <= testCase; t++){
-  let n = Number(input[t]);
-
-  if(n === 0) console.log(1, 0);
-  else console.log(d[n - 1], d[n]);
+for(let i = 1; i <= testCase; i++){
+  let number = Number(input[i]);
+  if(number === 0) console.log(1, 0);
+  else console.log(dp[number - 1], dp[number])
 }
