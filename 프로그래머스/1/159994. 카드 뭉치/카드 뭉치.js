704251f85e76826619026c7bfde91e899b678035
jaehoon ahn <jaehun4086@naver.com>
2024-10-23 12:47:08 +0900

    [level 1] Title: 카드 뭉치, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.js
@@ -1,22 +1,32 @@
 function solution(cards1, cards2, goal) {
-    var answer = 'x';
-    let length = cards1.length > cards2.length ? cards1.length : cards2.length;
+    let cardsOne = [];
+    let cardsTwo = [];
     for(let i = 0; i < goal.length; i++){
-        if(cards1[0] === goal[i] || cards2[0] === goal[i]){
-            if(cards1 && cards1[0] === goal[i]){
-                answer = "o";
-                cards1.shift();
-            }
-            if(cards2 && cards2[0] === goal[i]){
-                answer = "o";
-                cards2.shift();
+        for(let j = 0; j < cards1.length; j++){
+            if(goal[i] === cards1[j]){
+                cardsOne.push(j);
             }
         }
-        else{
-            return "No";
+        for(let j = 0; j < cards2.length; j++){
+            if(goal[i] === cards2[j]){
+                cardsTwo.push(j);
+            }
         }
-            
     }
-    if(answer === "o")
-        return "Yes";
+    let flag = false;
+    for(let i = 0; i < cardsOne.length; i++){
+        if(i !== cardsOne[i]){
+            flag = true;
+            return "No"
+        }
+    }
+    for(let i = 0; i < cardsTwo.length; i++){
+        if(i !== cardsTwo[i]){
+            flag = true;
+            return "No"
+        }
+    }
+    if(!flag) return "Yes"
+    // console.log(cardsOne, cardsTwo)
+    
 }
