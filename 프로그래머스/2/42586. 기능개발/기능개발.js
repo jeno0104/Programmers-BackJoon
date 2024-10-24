eccc380ac4cb8a0f7899a93d5aabc583ebc21ecc
jaehoon ahn <jaehun4086@naver.com>
2024-10-24 12:51:05 +0900

    [level 2] Title: 기능개발, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,36 +1,59 @@
 function solution(progresses, speeds) {
     var answer = [];
-    let work_end_date = [];
-    let number_of_deployment = 1;
-    let discrimination = [];
-    
-    
-    for(let i = 0; i < progresses.length; i++){
-        work_end_date.push(Math.ceil((100 - progresses[i]) / speeds[i]));
-    }
-    
-    for(let i = 0; i < work_end_date.length; i++){
-        discrimination.push(true);
-    }
-    for(let i = 0; i < work_end_date.length - 1; i++){
-        for(let j = i + 1; j < work_end_date.length; j++){
-            if(discrimination[i] === false){
-                break;
-            }
-            if(work_end_date[i] >= work_end_date[j] && discrimination[j] !== false){
-                number_of_deployment += 1;
-                discrimination[j] = false;
-            }
-            else{
-                answer.push(number_of_deployment);
-                number_of_deployment = 1;
-                discrimination[i] = false;
-                break;
-            }
+    let summary
+//     while(progresses.length !== 0){
+//         let cnt = 0;
+//         let flag = false;
+//         for(let i = 0; i < progresses.length; i++){
+//             if(progresses[0] < 100){
+//                 progresses[i] += speeds[i];
+//                 flag = false;
+//             }
+//             else if(progresses[0] >= 100){
+//                 flag = true;
+//                 progresses.shift()
+//                 speeds.shift();
+//                 cnt++;
+//                 continue;
+//             }
+//         }
+//         if(flag){
             
+//         }
+//         console.log(progresses)
+//         console.log(cnt)
+//         console.log("====================")
+//     }
+    let newPro = [];
+    for(let i = 0; i < progresses.length; i++){
+        newPro.push(100 - progresses[i]);
+    }
+    let target = 0;
+    let i = 1;
+    
+    let result = [];
+    for(let i = 0; i < newPro.length; i++){
+        
+        result.push(Math.ceil(newPro[i] / speeds[i]))
+    }
+    let maxValue = result[0];
+    
+    let start = 0;
+    let end = 1;
+    let cnt = 1;
+    while(end < result.length){
+        if(result[start] < result[end]){
+            start = end;
+            end = start + 1;
+            answer.push(cnt);
+            cnt = 1;
+        }
+        else if(result[start] >= result[end]){
+            cnt++;
+            end++;
         }
     }
-    answer.push(number_of_deployment);
+    answer.push(cnt)
     
     return answer;
 }
