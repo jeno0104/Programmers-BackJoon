45f6d4c575c9fbd71d6ac0a64a2d6b1864f4da9e
jaehoon ahn <jaehun4086@naver.com>
2024-10-31 14:30:00 +0900

    [Silver II] Title: 차이를 최대로, Time: 176 ms, Memory: 10384 KB -BaekjoonHub

---
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.js
@@ -0,0 +1,26 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let n = Number(input[0]);
+let arr = input[1].split(" ").map(Number);
+let visited = new Array(n + 1).fill(false);
+let result = [];
+let maxValue = 0;
+dfs(0);
+
+function dfs(depth){
+  if(depth === n){
+    let current = 0;
+    for(let i = 0; i < n - 1; i++) current += Math.abs(result[i] - result[i + 1]);
+    maxValue = Math.max(maxValue, current);
+  }
+  for(let i = 0; i < n; i++){
+    if(visited[i]) continue;
+    visited[i] = true;
+    result.push(arr[i]);
+    dfs(depth + 1);
+    visited[i] = false;
+    result.pop();
+  }
+}
+console.log(maxValue);
